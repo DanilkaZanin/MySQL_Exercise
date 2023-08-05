c1f68250fe11b3e1437a20659e18745d291c6a57
danila <y-zelenov@mail.ru>
2023-08-05 17:16:37 +0300

    updated

--- src/main/java/org/example/AnimalConnection.java
+++ src/main/java/org/example/AnimalConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class AnimalConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Marina2009";
+    private static final String PASSWORD = "root";
 
     private static Connection connection;
 
